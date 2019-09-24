52344929018a6dba8a7464f07200ba06f3cd846c
sankeerthankam <sankeerth.ankam@gmail.com>
2019-09-24 16:01:40 -0700

    Create 2. Datatypes.py

---
+++ Hacker Rank/1. Basics/2. Datatypes.py
@@ -0,0 +1,22 @@
+# Create and add variables from program input and user input
+
+
+# Program Inputs - # Declare second integer, double, and String variables.
+i = 4
+j = 4.0
+k = 'HackerRank '
+
+# Read and save an integer, double, and String to your variables.
+ii = int(input())
+jj = float(input())
+kk = str(input())
+
+# Print the sum of both integer variables on a new line.
+print(i + ii)
+
+# Print the sum of the double variables on a new line.
+print(j + jj)
+
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
+print(k + kk)
