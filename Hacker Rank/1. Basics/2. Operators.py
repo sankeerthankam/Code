a0c31b95438defe65a0574a01f613e2c7af17fab
sankeerthankam <sankeerth.ankam@gmail.com>
2019-09-26 16:19:27 -0700

    Create 2. Operators.py

---
+++ Hacker Rank/1. Basics/2. Operators.py
@@ -0,0 +1,25 @@
+# Task:
+# Given the meal price (base cost of a meal), tip percent (the percentage of the meal price being added as tip), and tax percent (the percentage of the meal price being added as tax) for a meal, find and print the meal's total cost.
+# Note: Be sure to use precise values for your calculations, or you may end up with an incorrectly rounded result!
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the solve function below.
+def solve(meal_cost, tip_percent, tax_percent):
+    tip = meal_cost * (tip_percent/100)
+    tax = meal_cost * (tax_percent/100)
+    total = meal_cost + tip + tax
+    print(int(round(total)))
+
+if __name__ == '__main__':
+    meal_cost = float(input())
+    tip_percent = int(input())
+    tax_percent = int(input())
+
+    solve(meal_cost, tip_percent, tax_percent)
