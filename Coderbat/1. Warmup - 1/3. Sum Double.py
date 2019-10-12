5e046e5159296066bb5d80a11f3a46bd0146d935
sankeerthankam <sankeerth.ankam@gmail.com>
2019-10-12 10:28:33 -0700

    Create 3. Sum Double.py

---
+++ Coderbat/1. Warmup - 1/3. Sum Double.py
@@ -0,0 +1,9 @@
+# Given two int values, return their sum. Unless the two values are the same, then return double their sum.
+
+def sum_double(a, b):
+  if a == b:
+    return 2*(a+b)
+  else:
+    return a + b
+
+sum_double(3, 3)
