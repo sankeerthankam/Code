8133cee440f083e03a01f188a6556853dc580344
sankeerthankam <sankeerth.ankam@gmail.com>
2019-10-15 10:57:13 -0700

    Create 11. Front Back.py

---
+++ Coderbat/1. Warmup - 1/11. Front Back.py
@@ -0,0 +1,7 @@
+# Given a string, return a new string where the first and last chars have been exchanged.
+
+def front_back(str):
+  if len(str) > 1:
+    return str[-1] + str[1:-1] + str[0]
+  else:
+    return str
