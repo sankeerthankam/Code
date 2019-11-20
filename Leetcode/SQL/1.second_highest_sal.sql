7c1f2bef32d0b65fad42066086f4d24926ff2049
sankeerthankam <sankeerth.ankam@gmail.com>
2019-11-20 15:00:31 -0800

    Update 1.second_highest_sal.sql

--- Leetcode/SQL/1.second_highest_sal.sql
+++ Leetcode/SQL/1.second_highest_sal.sql
@@ -1,2 +1,4 @@
 # Write your MySQL query statement below
-select salary as SecondHighestSalary from Employee order by salary desc limit 1, 1
+
+select max(salary) as SecondHighestSalary from Employee where salary < (select max(salary) from Employee) limit 1
+-- select salary as SecondHighestSalary from Employee order by salary desc limit 1, 1
