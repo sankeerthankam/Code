b17d08977acd7090e16d469afddcddbc6cc3921d
sankeerthankam <sankeerth.ankam@gmail.com>
2019-12-25 17:12:30 -0800

    Update 1. Two Sum.py

--- Leetcode/1. F/1. Two Sum.py
+++ Leetcode/1. F/1. Two Sum.py
@@ -4,10 +4,9 @@
 # For example: target_sum = 9, nums = [2, 7, 11, 15]; 
 # Here i = 2, required/complement = 7 -> So, we look for 7 in the rest of the list
 # When required element is found, return the indices from visited_list
-
+# Note: Might work only on a sorted list
 
 def twoSum(self, nums: List[int], target: int) -> List[int]:
-    nums = sorted(nums)
     visited_elements = {}
     for i in range(len(nums)):
         required = target - nums[i]
