6452c765df41c209ecb7d03192607943b1b86e5c
sankeerthankam <sankeerth.ankam@gmail.com>
2019-12-26 15:17:26 -0800

    Update and rename 125.valid_palindrome.py to 125. Valid Palindrome.py

---
+++ Leetcode/1. F/125. Valid Palindrome.py
@@ -0,0 +1,7 @@
+def isPalindrome(self, string: str) -> bool:
+    string = string.lower()
+    string = re.sub('[^a-z0-9]', '', string)
+    if string == string[::-1]:
+        return True
+    else:
+        return False
--- Leetcode/1. F/125.valid_palindrome.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def isPalindrome(self, string: str) -> bool:
-        string = string.lower()
-        string = re.sub('[^a-z0-9]', '', string)
-        if string == string[::-1]:
-            return True
-        else:
-            return False
