69a26a7c9742b8b3c27fbaf223deb6069958d8ff
sankeerthankam <sankeerth.ankam@gmail.com>
2020-02-18 10:49:23 -0800

    Update 201. Bitwise AND of Numbers Range.py

--- Leetcode/Bootcamp/Week 1/201. Bitwise AND of Numbers Range.py
+++ Leetcode/Bootcamp/Week 1/201. Bitwise AND of Numbers Range.py
@@ -1,3 +1,8 @@
+# Given two numbers m and n
+# If n == m, return 0
+# If n = m + 1, return n
+# Else for all numbers between m and n, perform AND operation between them and return the result
+
 def rangeBitwiseAnd(m, n):
     
     if m - n <= 1:
