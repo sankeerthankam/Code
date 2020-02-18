6729a228298c2200f76f9e81d61926a6b0a1475f
sankeerthankam <sankeerth.ankam@gmail.com>
2020-02-18 11:05:07 -0800

    Delete 136. Single Number.py

--- Leetcode/Bootcamp/Week 1/136. Single Number.py
+++
@@ -1,8 +0,0 @@
-# Use counter object to count the character occurances
-# Use most_common() to sort the keys based on values
-# Return the last key
-
-def singleNumber(self, nums: List[int]) -> int:
-    from collections import Counter
-    c1 = Counter(nums)
-    return [i for i in c1.most_common()[-1]][0]
