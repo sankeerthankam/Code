eeaf1a641f97df4de1eb2de1cafa1515ef40a3c5
sankeerthankam <sankeerth.ankam@gmail.com>
2020-02-23 11:24:04 -0800

    Update 137. Single Number II.py

--- Leetcode/Bootcamp/Week 1/137. Single Number II.py
+++ Leetcode/Bootcamp/Week 1/137. Single Number II.py
@@ -1,7 +1,13 @@
+# Approach 1:
+# Use counter to count the occurances
+# RTeturn the least common key
+
 def singleNumber(self, nums: List[int]) -> int:
     from collections import Counter
     c1 = Counter(nums)
     return [i for i in c1.most_common()[-1]][0]
 
+# Approach 2:
+# The reason we subtract the result by 2 is becuase all the digits occur thrice and one of them occurs only once (so the diffrence should be divided by 2 to get the digit)
 def singleNumber(self, nums: List[int]) -> int:
   return (3*sum(list(set(A)))-sum(A))/2
