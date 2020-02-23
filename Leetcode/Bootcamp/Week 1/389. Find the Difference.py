2c85ca52c34d47a453f21af14b46c071e8fef1df
sankeerthankam <sankeerth.ankam@gmail.com>
2020-02-23 09:02:30 -0800

    Update 389. Find the Difference.py

--- Leetcode/Bootcamp/Week 1/389. Find the Difference.py
+++ Leetcode/Bootcamp/Week 1/389. Find the Difference.py
@@ -1,3 +1,4 @@
+# Approach 1:
 # Set comparision would not be useful because set does not take duplicate characters into consideration.
 # i.e. 'aa' is the same as 'a'
 # Use Couter object here to get the count of each character in each word
@@ -10,14 +11,13 @@ def findTheDifference(s, t):
 
     return ''.join([i[0] for i in c2 - c1])
 
-# Sort the lists of two strings
-# Traverse and compare each character 
-# Return the char where there is a difference
+# Approach 2:
+# Sort the strings -> This returns a list
+# Since the first string is 1 letter short of second string, add [''] to the first sorted list
+# Zip two lists and compare each elements
+# When there is a mismatch, return the char from the second list
 
-def findTheDifference(s, t):
-    s = sorted(list(s))
-    t = sorted(list(t))
-    for i in range(len(s)):
-        if s[i]!=t[i]:
-            return t[i]
-    return t[-1]
+def diff(a, b):
+    for i, j in zip(sorted(a) + [''], sorted(b)):
+        if i != j:
+            return j
