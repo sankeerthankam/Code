986be9ad4bf10a153e76c1e2598fcb73531ee3cf
sankeerthankam <sankeerth.ankam@gmail.com>
2020-02-23 09:19:56 -0800

    Update 136. Single Number.py

--- Leetcode/Bootcamp/Week 1/136. Single Number.py
+++ Leetcode/Bootcamp/Week 1/136. Single Number.py
@@ -5,19 +5,6 @@
 # Sort the counter object using .most_common()
 # Return the element from the last
 
-# Approach 2:
-# Extract the unique elements from the list using the set()
-# Twice the sum of the set
-# Subtract the sum of the nums from sum of the set
-
-# Approach 3:
-# XOR Operation works similarly for bits as well as integers
-# a ^ a = 0
-# a ^ b = 1
-# XOR of two similar elemets is 0 
-# XOR of two different elemets is 1
-# We do the same to the given list of elements -> It cancels out the similar elements and return the unique one
-
 # Approach 4:
 # Sort the numbers
 # Assign +, - sign consecutively 
@@ -28,8 +15,21 @@ def singleNumber(nums):
     c1 = Counter(nums)
     return [i for i in c1.most_common()[-1]][0]
 
+# Approach 2:
+# Extract the unique elements from the list using the set()
+# Twice the sum of the set
+# Subtract the sum of the nums from sum of the set
+
 def sol2(nums):
     return 2*sum(set(nums)) - sum(nums)
+
+# Approach 3:
+# XOR Operation works similarly for bits as well as integers
+# a ^ a = 0
+# a ^ b = 1
+# XOR of two similar elemets is 0 
+# XOR of two different elemets is 1
+# We do the same to the given list of elements -> It cancels out the similar elements and return the unique one
 
 def sol3(nums):
     # https://www.youtube.com/watch?v=vQbrPU4hW3c
