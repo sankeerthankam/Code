f95e6e863181f01ada369b532b8f2dff51c15cf2
sankeerthankam <sankeerth.ankam@gmail.com>
2020-02-23 09:47:46 -0800

    Update 268. Missing Number.py

--- Leetcode/Bootcamp/Week 1/268. Missing Number.py
+++ Leetcode/Bootcamp/Week 1/268. Missing Number.py
@@ -1,3 +1,6 @@
+# Approach 1:
+# Create a list of len(l) 
+# Traverse through the list and return the missing number
 def missingNumber(nums):
     if len(nums) == 0:
         return None
@@ -7,7 +10,18 @@ def missingNumber(nums):
         for i in range(len(nums)+1):
             if i not in nums:
                 return i
-                
+
+            
+# Approach 2:
+# Create a list of len(l) 
+# Calculate the sum of new_list
+# Subtract the sum of input list from sum of new_list
+# Return the difference
+
+def missing(nums):
+    return sum([i for i in range(len(nums)+1)]) - sum(nums)
+    
+# Approach 3:
 # Find the sum of n numbers 
 # Subtract the sum of the current array
 # Type caset the result to int and return the result
