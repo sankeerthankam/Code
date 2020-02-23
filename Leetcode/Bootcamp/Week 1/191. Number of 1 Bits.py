63f5abc3beaea62120bbc4935b6060e523472413
sankeerthankam <sankeerth.ankam@gmail.com>
2020-02-23 10:48:43 -0800

    Update 191. Number of 1 Bits.py

--- Leetcode/Bootcamp/Week 1/191. Number of 1 Bits.py
+++ Leetcode/Bootcamp/Week 1/191. Number of 1 Bits.py
@@ -1,5 +1,16 @@
+# Approach 1:
 # Convert the integer to binary and then into string
 # Return the count of 1s
 
 def hammingWeight(n):
     return str(bin(n)).count('1')
+
+# Approach 2:
+# https://www.youtube.com/watch?v=KJnhAUkxAho
+# Use bit manipulation as shown in the above video
+def  countSetBits(n): 
+    count = 0
+    while (n): 
+        count += n & 1
+        n >>= 1
+    return count 
