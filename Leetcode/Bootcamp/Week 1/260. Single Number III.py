23c37fd02c91d47ddc53e615f452fe2a3164a216
sankeerthankam <sankeerth.ankam@gmail.com>
2020-02-23 11:34:39 -0800

    Update 260. Single Number III.py

--- Leetcode/Bootcamp/Week 1/260. Single Number III.py
+++ Leetcode/Bootcamp/Week 1/260. Single Number III.py
@@ -1,4 +1,37 @@
+# Approach 1:
+# Use counter object
 def singleNumber(nums):
     from collections import Counter
     c1 = Counter(nums)
     return [i[0] for i in c1.most_common()[-2:]]
+
+
+# Approach 2:
+# Naive Solution
+# Keep track of num of occurances of each element
+
+def singleNumber(nums):
+    one = set()
+    double = set()
+    for n in nums:
+        if n not in one:
+            one.add(n)
+        else:
+            double.add(n)
+
+    return list(one - double)
+
+
+# Approach 3:
+# Naive Solution (Improved)
+# Keep track of num of occurances of each element
+
+def singleNumber(nums):
+    one = set()
+    for n in nums:
+        if n not in one:
+            one.add(n)
+        else:
+            one.remove(n)
+
+    return list(one)
