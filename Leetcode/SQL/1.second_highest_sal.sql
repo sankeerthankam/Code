05d84c277cfde9083b15d22f8c43bf7b8ce3a0c2
sankeerthankam <sankeerth.ankam@gmail.com>
2020-03-12 00:03:48 -0700

    Update 1.second_highest_sal.sql

--- Leetcode/SQL/1.second_highest_sal.sql
+++ Leetcode/SQL/1.second_highest_sal.sql
@@ -10,4 +10,30 @@ select a.Salary as SecondHighestSalary from (select Salary from Employee order b
 -- Approach 1 does not return null 
 
 select max(salary) as SecondHighestSalary from Employee where salary < (select max(salary) from Employee) limit 1
--- select salary as SecondHighestSalary from Employee order by salary desc limit 1, 1
+
+
+-- Run the below query to get third highest, Same query is applicable for nth highest salary just change at rownum<=n-1 .
+-- Approach:
+  -- Order the salaries
+  -- Assign a row num to all the salaries
+  -- Extract subset of salaries where row_num is < 3 [This gives top 2 salaries]
+  -- Now, extract max(salary) from Employee table where the salary is not in above sub_table.
+  
+  -- Example:
+  -- Actual Salaries: [100, 200, 110, 120, 300]
+  -- Ordered Salaries: [300, 200, 120, 110, 100]
+  -- {
+      1: 300,
+      2: 200,
+      3: 120,
+      4: 110,
+      5: 100
+  -- }
+  -- Subset of top 2 salaries: [300, 200]
+  -- max([120, 110, 100]) 
+  -- 120
+select max(salary) from employee
+where salary not in (
+  select salary from 
+    (select DISTINCT salary from employee order by salary desc)
+where rownum<=2);
