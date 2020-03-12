89c70be998aba7d3563414c917a2b685f9c059d1
sankeerthankam <sankeerth.ankam@gmail.com>
2020-03-11 23:46:38 -0700

    Update 1.second_highest_sal.sql

--- Leetcode/SQL/1.second_highest_sal.sql
+++ Leetcode/SQL/1.second_highest_sal.sql
@@ -1,4 +1,13 @@
-# Write your MySQL query statement below
+-- Approach 1
+-- Order salary descending and limit 2
+-- Order salary ascending from above derived table and Select the second highest salary 
+
+select a.Salary as SecondHighestSalary from (select Salary from Employee order by Salary desc limit 2) a order by salary asc limit 1
+
+-- Approach 2
+-- Above approach works only when there are two highest salaries
+-- What if there are nulls?
+-- Approach 1 does not return null 
 
 select max(salary) as SecondHighestSalary from Employee where salary < (select max(salary) from Employee) limit 1
 -- select salary as SecondHighestSalary from Employee order by salary desc limit 1, 1
