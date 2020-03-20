c48493cafa3306d5c5bfbb3e3c2001312dd739ea
sankeerthankam <sankeerth.ankam@gmail.com>
2020-03-20 09:50:41 -0700

    Update 2.nth_highest_sal.sql

--- Leetcode/SQL/2.nth_highest_sal.sql
+++ Leetcode/SQL/2.nth_highest_sal.sql
@@ -1,9 +1,26 @@
+-- Approach 1
+-- Naìve Approach
+
+SELECT TOP 1 Salary
+FROM (
+      SELECT DISTINCT TOP N Salary
+      FROM Employee
+      ORDER BY Salary DESC
+      ) AS Emp
+ORDER BY Salary
+
+
+-- Approach 2
+-- My SQL
+SELECT Salary FROM Employee ORDER BY Salary DESC LIMIT n-1,1
+
+-- Approach 3
+-- Using Rank
 CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
 BEGIN
 DECLARE M INT;
 SET M = N-1;
   RETURN (
-      # Write your MySQL query statement below.
       select DISTINCT salary from Employee order by salary desc limit M, 1
   );
 END
