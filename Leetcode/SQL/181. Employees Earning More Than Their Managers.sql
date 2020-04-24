8a0ec4f0506e4a35fec9b5e42b0e3f3d98e4e284
sankeerthankam <sankeerth.ankam@gmail.com>
2020-04-23 18:37:08 -0700

    Update 181. Employees Earning More Than Their Managers.sql

--- Leetcode/SQL/181. Employees Earning More Than Their Managers.sql
+++ Leetcode/SQL/181. Employees Earning More Than Their Managers.sql
@@ -19,3 +19,7 @@ from
 where 
     e1.ManagerId = e2.Id
     AND e1.Salary > e2.Salary
+
+-- Improvements:
+-- The above approach scans the atble twice - Instead you can extract the list of managers beforehand using distinct and id != numm (this eliminates duplicate and null records)
+-- https://leetcode.com/problems/employees-earning-more-than-their-managers/discuss/406403/Faster-than-96-Using-subquery
