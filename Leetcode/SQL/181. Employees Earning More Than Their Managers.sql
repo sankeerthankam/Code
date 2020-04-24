1ca753d033dfaab6d9b416218ed8e77bc10386c8
sankeerthankam <sankeerth.ankam@gmail.com>
2020-04-23 18:31:40 -0700

    Update 181. Employees Earning More Than Their Managers.sql

--- Leetcode/SQL/181. Employees Earning More Than Their Managers.sql
+++ Leetcode/SQL/181. Employees Earning More Than Their Managers.sql
@@ -1,5 +1,16 @@
 # Write your MySQL query statement below
 
+SELECT 
+    e1.Name as Employee
+FROM 
+    Employee e1
+LEFT JOIN 
+    Employee e2
+ON 
+    e1.ManagerId = e2.Id
+WHERE 
+    e1.Salary > e2.Salary
+------------------------------------------------------------------------
 
 select 
     e1.Name as Employee -- , e1.Salary, e1.ManagerId, e2.Id, e2.Name as Manager, e2.Salary as ManagerSal
