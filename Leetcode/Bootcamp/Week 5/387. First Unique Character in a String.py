d5dc5c4b2349ce5644eb4be8dd5d8062bc290bac
sankeerthankam <sankeerth.ankam@gmail.com>
2020-04-28 11:32:46 -0700

    Update 387. First Unique Character in a String.py

--- Leetcode/Bootcamp/Week 5/387. First Unique Character in a String.py
+++ Leetcode/Bootcamp/Week 5/387. First Unique Character in a String.py
@@ -1,7 +1,17 @@
+# Approach 1:
+# For each char in the string, traverse therough the string again to check its second occurance
+# This hass O(N^2) time-complexity
 def firstUniqChar(s):
     l = len(s)
     for i in range(l):
         ch = s[i]
         if ch not in s[:i]+s[i+1:]:
             return i
+    return -1
+
+# Approach 2:
+# User counter from itertools (COunter preserves the order)
+def firstUniqChar(s):
+    for i,j in Counter(s).items(): 
+        if j == 1: return(s.index(i))
     return -1
