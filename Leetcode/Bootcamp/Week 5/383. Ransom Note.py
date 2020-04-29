4902e6e63a740f6ae302c949d3a9262720234fdf
sankeerthankam <sankeerth.ankam@gmail.com>
2020-04-29 15:25:28 -0700

    Update 383. Ransom Note.py

--- Leetcode/Bootcamp/Week 5/383. Ransom Note.py
+++ Leetcode/Bootcamp/Week 5/383. Ransom Note.py
@@ -8,3 +8,16 @@ def canConstruct(ransomNote, magazine):
         if i not in c2 or c1.get(i) > c2.get(i):
             return False
     return True
+
+
+def canConstruct(ransomNote, magazine):
+
+    from collections import Counter
+
+    c1 = Counter(ransomNote)
+    c2 = Counter(magazine)
+
+    if c1 - c2 == 0:
+        return True
+    else:
+        return False
