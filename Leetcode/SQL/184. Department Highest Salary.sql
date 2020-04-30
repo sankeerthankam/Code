edadebbacd1d19240f9d12fbf3fd8b1dc2dd21b8
sankeerthankam <sankeerth.ankam@gmail.com>
2020-04-30 16:25:42 -0700

    Update 184. Department Highest Salary.sql

--- Leetcode/SQL/184. Department Highest Salary.sql
+++ Leetcode/SQL/184. Department Highest Salary.sql
@@ -9,7 +9,6 @@ SELECT
     t3.Name as Department, t1.Name as Employee, t1.Salary
 FROM 
     Employee t1,
-
     (SELECT 
         DepartmentId, MAX(Salary) as Salary
     FROM
@@ -31,10 +30,10 @@ WHERE
 --   (dempartment_id, salary) = (department_id, max(salary)
 SELECT 
     t2.Name AS Department, t1.Name AS Employee, t1.Salary 
-from 
-	Employee t1,
-	Department t2 
+FROM 
+    Employee t1,
+    Department t2 
 WHERE 
     t1.DepartmentId = t2.id 
-    AND (DepartmentId, Salary) in 
+    AND (DepartmentId, Salary) IN
     (SELECT DepartmentId, max(Salary) FROM Employee GROUP BY 1)
