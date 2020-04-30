bf26b69153d9288072e99b2909161d6a61fd5965
sankeerthankam <sankeerth.ankam@gmail.com>
2020-04-30 16:15:04 -0700

    Update 184. Department Highest Salary.sql

--- Leetcode/SQL/184. Department Highest Salary.sql
+++ Leetcode/SQL/184. Department Highest Salary.sql
@@ -1,13 +1,10 @@
-# Approach 1
-  # Employee (Employee Name)
-  #  LEFT JOIN
-  # Employee (Department and Max Salary)
-  #  LEFT JOIN
-  # Department (Department Name)
-# Cons: Less Than 5% time complexity
-
-# Write your MySQL query statement below
-
+-- Approach 1
+  -- Employee (Employee Name)
+  --  LEFT JOIN
+  -- Employee (Department and Max Salary)
+  --  LEFT JOIN
+  -- Department (Department Name)
+-- Cons: Less Than 5% time complexity
 SELECT
     t3.Name as Department, t1.Name as Employee, t1.Salary
 FROM 
@@ -19,10 +16,25 @@ FROM
         Employee
     GROUP BY 
         1) t2,
-    
     Department t3
-
 WHERE 
     t1.DepartmentId = t2.DepartmentId 
     AND t1.Salary = t2.Salary
     AND t1.DepartmentId = t3.Id
+    
+    
+    
+-- Approach 2
+-- Employee 
+  -- LEFT JOIN 
+-- Department
+--   (dempartment_id, salary) = (department_id, max(salary)
+SELECT 
+    t2.Name AS Department, t1.Name AS Employee, t1.Salary 
+from 
+	Employee t1,
+	Department t2 
+WHERE 
+    t1.DepartmentId = t2.id 
+    AND (DepartmentId, Salary) in 
+    (SELECT DepartmentId, max(Salary) FROM Employee GROUP BY 1)
