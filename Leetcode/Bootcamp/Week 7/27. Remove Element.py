48157b9bf141235a664a110c076eac6891156488
sankeerthankam <sankeerth.ankam@gmail.com>
2020-05-11 10:28:47 -0700

    Update 27. Remove Element.py

--- Leetcode/Bootcamp/Week 7/27. Remove Element.py
+++ Leetcode/Bootcamp/Week 7/27. Remove Element.py
@@ -1,4 +1,9 @@
+
 def removeElement(nums, val):
     while val in nums:
         nums.remove(val)
     return len(nums)
+
+# Resources: 
+# https://stackoverflow.com/questions/7118276/how-to-remove-specific-element-from-an-array-using-python
+# https://stackoverflow.com/questions/11520492/difference-between-del-remove-and-pop-on-lists/11520540
