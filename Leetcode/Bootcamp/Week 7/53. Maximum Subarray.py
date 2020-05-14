3db657602e2e1034de85cc4263fec6461fac5882
sankeerthankam <sankeerth.ankam@gmail.com>
2020-05-14 11:16:19 -0700

    Update 53. Maximum Subarray.py

--- Leetcode/Bootcamp/Week 7/53. Maximum Subarray.py
+++ Leetcode/Bootcamp/Week 7/53. Maximum Subarray.py
@@ -18,3 +18,19 @@ def maxSubArray(arr):
       current_sum = max(arr[i], current_sum+arr[i])
       max_sum = max(max_sum, current_sum)
   return max_sum
+
+# Rewriting the code for the above approach to improve time complexity
+# Do not use extra variable to calculate the len(array)
+# Initialize variable only if edge case fails
+# Update `max_sum` only when current_max > max_sum
+
+def maxSubArray(nums):
+  if len(nums) == 1:
+      return nums[0]
+  max_sum = current_max = nums[0]
+  for i in nums[1:]:
+      current_max = max(i, current_max+i)
+      # max_sum = max(max_sum, current_max)
+      if current_max > max_sum: 
+          max_sum = current_max
+  return max_sum    
