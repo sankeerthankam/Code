34c18b02b0ae3f32bfea934cbc5d1872f6b56273
sankeerthankam <sankeerth.ankam@gmail.com>
2020-05-17 13:59:47 -0700

    Update 231. Power of Two.py

--- Leetcode/Bootcamp/Week 1/231. Power of Two.py
+++ Leetcode/Bootcamp/Week 1/231. Power of Two.py
@@ -7,7 +7,6 @@ def isPowerOfTwo(n):
     import math
     return round(math.log(n, 2), 11).is_integer()
     
-# If a number is a power of two - then it should have only single occurance of 1 in its binary representation
-
+# If a number is a power of two - then it would have only single occurance of 1 in its binary representation
 def isPowerOfTwo(n):
     return (n > 0 and bin(n).count('1') == 1)
