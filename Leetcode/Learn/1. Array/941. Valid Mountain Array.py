5a4486102cecd8e7061449f490cbb1753de64edc
sankeerthankam <sankeerth.ankam@gmail.com>
2020-05-20 11:54:28 -0700

    Update 941. Valid Mountain Array.py

--- Leetcode/Learn/1. Array/941. Valid Mountain Array.py
+++ Leetcode/Learn/1. Array/941. Valid Mountain Array.py
@@ -1,3 +1,6 @@
+# Approach 1
+# Read the ccomments
+
 def validMountainArray(A):
   # Edge Case (Also this is one of the assumption)
   if len(A) < 3: 
@@ -22,3 +25,23 @@ def validMountainArray(A):
       else:
           return False
   return True
+
+# Approach 2
+# Use two pointers flag1 and flag2
+# Update flag1 = True at the end of the increment peak
+# Update flag2 = True at the end of the decrement peak
+# return (flag1 and flag2) which will return True if both are True
+
+def validMountainArray(A):
+  flag1 = False
+  flag2 = False
+  for i in range(len(A)-1):
+      if A[i]==A[i+1]:
+          return False
+      elif A[i]<A[i+1]:
+          if flag2==True:
+              return False
+          flag1=True
+      elif A[i]>A[i+1]:
+          flag2=True
+  return flag1 and flag2
