9c03985cf709fc707e981833d00e9242f8d811ff
sankeerthankam <sankeerth.ankam@gmail.com>
2020-05-26 15:47:45 -0700

    Update 253. Meeting Rooms II.py

--- Leetcode/Bootcamp/Week 9/253. Meeting Rooms II.py
+++ Leetcode/Bootcamp/Week 9/253. Meeting Rooms II.py
@@ -1,3 +1,21 @@
+# Approach 1
+# Compare start and end times 
+# But this approach doesn't work for overlapping start and end times
+def minMeetingRooms(intervals):
+  intervals.sort()
+  count = 1
+  for i in range(len(intervals)-1):
+      if intervals[i][1] > intervals[i+1][0]:
+          count = count + 1
+  return count
+ 
+# Approach 2
+ # Very similar with what we do in real life. Whenever you want to start a meeting, 
+ # you go and check if any empty room available (available > 0) and
+ # if so take one of them ( available -=1 ). Otherwise,
+ # you need to find a new room someplace else ( numRooms += 1 ).  
+ # After you finish the meeting, the room becomes available again ( available += 1 ).
+
 def minMeetingRooms(intervals):
   starts = []
   ends = []
