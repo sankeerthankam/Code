ae87ab2df7ff06adb739d5a5bcaa1caba17cd9fc
sankeerthankam <sankeerth.ankam@gmail.com>
2025-10-28 06:16:33 +0530

    Create 1. Two Sum.py
    1. Two Sum

---
+++ Leetcode/Learn/1. Array/1. Two Sum.py
@@ -0,0 +1,9 @@
+def twoSum(self, nums: List[int], target: int) -> List[int]:
+    lookup_map = {}
+    for i, j in enumerate(nums):
+        lookup_val = target - j
+        if lookup_val in lookup_map:
+            return [lookup_map[lookup_val], i]
+        else:
+            lookup_map[lookup_val] = i
+    return
