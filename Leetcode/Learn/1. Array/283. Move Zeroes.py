9dba327615d8efd40bfa9115ddd0cb034871c53d
sankeerthankam <sankeerth.ankam@gmail.com>
2025-10-28 06:33:45 +0530

    Update 283. Move Zeroes.py

--- Leetcode/Learn/1. Array/283. Move Zeroes.py
+++ Leetcode/Learn/1. Array/283. Move Zeroes.py
@@ -1,8 +1,12 @@
 def moveZeroes(self, nums: List[int]) -> None:
     """
     Do not return anything, modify nums in-place instead.
+    Left pointer and Right pointer starts at 0
+    Right increments by 1 in the for loop
+    When right pointer encounters a non-zero element, we swap the right element with the left element
+        And incerement left pointer by 1 (since it has already been updated)
     """
-    l = 0
+    l = 0 
     for r in range(len(nums)):
         if nums[r] != 0:
             nums[l], nums[r] = nums[r], nums[l]
