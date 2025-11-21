945de1f4c71d2392b739503e5ea03602ccd1046d
sankeerthankam <sankeerth.ankam@gmail.com>
2025-11-21 12:42:25 +0530

    Create 1. Two Sum.py

---
+++ Leetcode/1. Arrays and Hashing/1. Two Sum.py
@@ -0,0 +1,10 @@
+# Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+# You may assume that each input would have exactly one solution, and you may not use the same element twice.
+# You can return the answer in any order.
+def twoSum(self, nums: List[int], target: int) -> List[int]:
+    lookup = {}
+    for i in range(len(nums)):
+        if target-nums[i] not in lookup:
+            lookup[nums[i]] = i
+        else:
+            return [lookup.get(target-nums[i]), i]
